a6e71edbe7b67aead492a9bc9887e917c88846ec
Aman Sachin Kujur <amansachinkujurgumla2005@gmail.com>
2021-05-29 18:26:37 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var audio1 = document.createElement('audio');
+audio1.setAttribute('src', 'sound.mp3');
+audio1.loop = true;
+audio1.play();
+
+
+
+var audio2 = document.createElement('audio');
+audio2.setAttribute('src', 'jetaudio.mp3');
+audio2.loop = true;
+audio2.play();
+
